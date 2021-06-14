32c833fe77b640e4b9519db79b2461dda622e4e8
Martijn18 <400105812@student.zadkine.nl>
2021-06-14 14:20:31 +0200

    Create triggerai.cs

---
+++ game/Assets/triggerai.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerai : MonoBehaviour
+{
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        if (hitInfo.tag == "Player")
+        {
+            animator.SetBool("IsFollowing", true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            animator.SetBool("IsFollowing", false);
+        }
+    }
+   
+
+}
